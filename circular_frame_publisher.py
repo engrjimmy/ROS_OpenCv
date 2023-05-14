7ce9ead5f9e1388f14c67fa917ff49ca6362820f
engrjimmy <eng.jimmy.majumder@gmail.com>
2023-05-14 13:36:41 +0900

    cv2 python code for circular shape in ros

---
+++ circular_frame_publisher.py
@@ -0,0 +1,29 @@
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+def main():
+    rospy.init_node("circular_frame_publisher")
+    pub = rospy.Publisher("/camera/circular_frame", Image, queue_size=10)
+    rate = rospy.Rate(10)  # Publish at 10 Hz
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+    bridge = CvBridge()
+    while not rospy.is_shutdown():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        height, width, channels = frame.shape
+        center_x, center_y = int(width/2), int(height/2)
+        scaling_factor = 0.5  # Change this to adjust the size of the circle
+        if width < height:
+            radius = int(width * scaling_factor / 2)
+        else:
+            radius = int(height * scaling_factor / 2)
+        cv2.circle(frame, (center_x, center_y), radius, (0, 255, 0), 2)
+        pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
+        rate.sleep()
+    cap.release()
+
+if __name__ == '__main__':
+    main()
---
+++ cv2_circular_frame.py
@@ -0,0 +1,26 @@
+import cv2
+
+def main():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        height, width, channels = frame.shape
+        center_x, center_y = int(width/2), int(height/2)
+
+        circule_scaling = 0.5
+
+        if width <height:
+            radius = int(hwidth * circule_scaling /2 )
+        else:
+            radius = int(height * circule_scaling/2)
+        cv2.circle(frame, (center_x, center_y), radius, (0, 100, 0), 2)
+        cv2.imshow("Circular Frame", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
